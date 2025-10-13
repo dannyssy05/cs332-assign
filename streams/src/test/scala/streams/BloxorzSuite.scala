a14caae96fddb3ec21cf85ac2d1d4a35852f40c7
dannyssy05 <dannyssy05@gmail.com>
2025-10-13 23:54:13 +0900

    streams done

--- streams/src/test/scala/streams/BloxorzSuite.scala
+++ streams/src/test/scala/streams/BloxorzSuite.scala
@@ -63,5 +63,79 @@ class BloxorzSuite extends FunSuite {
     new Level1 {
       assert(solution.length == optsolution.length)
     }
+
   }
+  test("TEST 1") {
+    new Level1 {
+      val input = Set(
+        (Block(Pos(1,2),Pos(1,3)), List(Right,Left,Up)),
+        (Block(Pos(2,1),Pos(3,1)), List(Down,Left,Up))
+      ).toStream
+
+      val exploredSet = Set(
+        Block(Pos(1,2),Pos(1,3)),
+        Block(Pos(1,1),Pos(1,1))
+      )
+
+      val expected = Set(
+        (Block(Pos(2,1),Pos(3,1)), List(Down,Left,Up))
+      ).toStream
+
+      val ret = newNeighborsOnly(input, exploredSet)
+      assert(ret === expected)
+    }
+  }
+
+  test("TEST 2") {
+    new Level1 {
+      val paths = from(Stream((startBlock, Nil)), Set(startBlock))
+      assert(paths.nonEmpty)
+    }
+  }
+
+  test("TEST 3") {
+    new Level1 {
+      val found = pathsFromStart.exists { case (b, _) => b.b1 == goal && b.b2 == goal }
+      assert(found)
+    }
+  }
+
+  trait Level0 extends SolutionChecker {
+    val level =
+      """------
+        |--ST--
+        |--oo--
+        |--oo--
+        |------""".stripMargin
+    val optsolution = List(Right, Down)
+  }
+
+  test("TEST 4") {
+    new Level0 {
+      assert(solve(solution) == Block(goal, goal))
+    }
+  }
+
+
+  test("TEST 6") {
+    new Level1 {
+      assert(solve(solution) == Block(goal, goal))
+    }
+  }
+
+  test("TEST 7") {
+    new Level1 {
+      assert(solution.length == optsolution.length)
+    }
+  }
+
+  test("TEST 8") {
+    new SolutionChecker {
+      val level =
+        """S-
+          |-T""".stripMargin
+      assert(solution == Nil)
+    }
+  }
+
 }
